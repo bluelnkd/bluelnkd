4f25e4ca49e3ea434c646416682496043958f52d
Stephen W <stephenw@bluelinked.com>
2019-04-23 21:25:11 -0700

    Update fullPage_new.js

--- lite/js/fullPage_new.js
+++ lite/js/fullPage_new.js
@@ -41,6 +41,7 @@ function createPayment(){
 
   fetch('https://paypal-integration-sample.herokuapp.com/api/paypal/ec/pd/create-payment/', {
       method: 'POST',
+      mode: 'no-cors',
       headers: {
         'Content-Type': 'application/json'
       },
